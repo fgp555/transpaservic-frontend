d0c1e95d4369fb2a6d0b6f7e1400ce82c920ce3b
ivanagbarreto <ivanagbarreto@gmail.com>
2025-01-31 19:06:00 -0300

    botones404-loader

--- src/components/ButtonStatus.jsx
+++ src/components/ButtonStatus.jsx
@@ -1,11 +1,69 @@
 import React from "react";
+import "./ButtonStatus.css";
 
 const ButtonStatus = () => {
   const status1 = "pendiente";
   const status2 = "cancelado";
   const status3 = "completado";
 
-  return <div>{status3 === "pendiente" && <button className="btn btn-primary">Pendiente</button>}</div>;
+  return (
+    <div className="ButtonStatus">
+      {status1 === "pendiente" && (
+        <button className="btn-warning">
+          {" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            width="5"
+            height="5"
+            className="dotButton"
+          >
+            <path
+              d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z"
+              fill="#CD6200"
+            />
+          </svg>{" "}
+          <span> Pendiente</span>
+        </button>
+      )}
+      {status2 === "cancelado" && (
+        <button className="btn-danger">
+          {" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            width="5"
+            height="5"
+            className="dotButton"
+          >
+            <path
+              d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z"
+              fill="#A30D11"
+            />
+          </svg>{" "}
+          <span> Cancelado</span>
+        </button>
+      )}
+      {status3 === "completado" && (
+        <button className="btn-success">
+          {" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            width="5"
+            height="5"
+            className="dotButton"
+          >
+            <path
+              d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z"
+              fill="#1F9254"
+            />
+          </svg>{" "}
+          <span> Completado</span>
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default ButtonStatus;
